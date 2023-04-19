85e692d90668be513a80b14b37d680e0b0dc236d
Thomas Weholt <2002@weholt.org>
2023-04-19 20:01:31 +0200

    Clean-up, moving functions unto utils.

--- wagtail_exifimage/utils.py
+++ wagtail_exifimage/utils.py
@@ -67,3 +67,59 @@ def get_basic_exif_data(filename):
                                 v = str(value)
                         result[field] = v
     return result
+
+
+def split_and_divide(string_value):
+    try:
+        v1, v2 = map(float, string_value.split("/"))  #: ['28/5'],
+        return v1 / v2
+    except:
+        return
+
+
+def transform_metadata(metadata):
+    result = {}
+    result["date_time_original"] = metadata.get("EXIF DateTimeOriginal", None)
+
+    result["shutter_speed"] = metadata.get("EXIF ExposureTime", "")
+    result["aperture"] = split_and_divide(
+        metadata.get("EXIF FNumber", "")
+    )  # metadata.get("EXIF FNumber"]  #: ['28/5'],
+    result["focal_length"] = metadata.get("EXIF FocalLength", "")
+    result["iso_rating"] = metadata.get("EXIF ISOSpeedRatings", "")
+    result["metering_mode"] = metadata.get("EXIF MeteringMode", "")
+
+    result["camera_make"] = metadata.get("Image Make", "")
+    result["camera_model"] = metadata.get("Image Model", "")
+    result["by_line"] = metadata.get("by-line", "")
+    result["caption"] = metadata.get("caption/abstract", "")
+    result["cateogry"] = metadata.get("category", "")
+    result["city"] = metadata.get("city", "")
+    result["copyright_notice"] = metadata.get("copyright notice", "")
+    result["country_iso_location_code"] = metadata.get(
+        "country/primary location code", None
+    )
+    result["country_location_name"] = metadata.get(
+        "country/primary location name", None
+    )
+    result["credit"] = metadata.get("credit", "")
+    result["state"] = metadata.get("custom11", "")
+    result["postal_code"] = metadata.get("custom12", "")
+    result["country"] = metadata.get("custom13", "")
+    result["phone"] = metadata.get("custom14", "")
+    result["email"] = metadata.get("custom15", "")
+    result["website"] = metadata.get("custom16", "")
+    result["address"] = metadata.get("custom9", "")
+    result["headline"] = metadata.get("headline", "")
+    result["title"] = metadata.get("headline", metadata.get("object name", "No title"))
+    result["keywords"] = metadata.get("keywords", "")
+    result["state"] = metadata.get("province/state", "")
+    result["source"] = metadata.get("source", "")
+    result["special_instructions"] = metadata.get("special instructions", "")
+    result["location"] = metadata.get("sub-location", "")
+
+    final_result = {}
+    for key in result:
+        if result[key]:
+            final_result[key] = result[key]
+    return final_result
